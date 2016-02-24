836d9e5a95c4329bbe939d0ef3337b5c00dc8128
xtuhcy <xtuhcy@163.com>
2016-02-24 10:38:28 +0800

    修复redirect的相对url的bug

---
+++ src/main/java/com/geccocrawler/gecco/utils/UrlUtils.java
@@ -0,0 +1,32 @@
+package com.geccocrawler.gecco.utils;
+
+import java.net.URL;
+
+public class UrlUtils {
+
+	/**
+	 * 相对地址转绝对地址
+	 * 
+	 * @param srcUrl
+	 * @param relativeUrl
+	 * @return
+	 */
+	public static String relative2Absolute(String absolutePath, String relativePath) {
+		if(relativePath == null) {
+			return null;
+		}
+		relativePath = relativePath.toLowerCase();
+		if(relativePath.startsWith("http")) {
+			return relativePath;
+		}
+		try {
+			// 以下方法对相对路径进行转换
+			URL absoluteUrl = new URL(absolutePath);
+			URL parseUrl = new URL(absoluteUrl, relativePath);
+			return parseUrl.toString();
+		} catch(Exception ex) {
+			return relativePath;
+		}
+	}
+
+}
