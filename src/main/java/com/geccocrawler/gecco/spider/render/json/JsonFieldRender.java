13b8c8200c155865d723453bb23e6d9b4f568753
xtuhcy <xtuhcy@163.com>
2016-04-20 22:57:25 +0800

    JsonBean支持嵌套解析

--- src/main/java/com/geccocrawler/gecco/spider/render/json/JsonFieldRender.java
+++ src/main/java/com/geccocrawler/gecco/spider/render/json/JsonFieldRender.java
@@ -1,7 +1,10 @@
 package com.geccocrawler.gecco.spider.render.json;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,6 +13,7 @@ import org.reflections.ReflectionUtils;
 import net.sf.cglib.beans.BeanMap;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.geccocrawler.gecco.annotation.JSONPath;
 import com.geccocrawler.gecco.request.HttpRequest;
 import com.geccocrawler.gecco.response.HttpResponse;
@@ -17,6 +21,11 @@ import com.geccocrawler.gecco.spider.SpiderBean;
 import com.geccocrawler.gecco.spider.conversion.Conversion;
 import com.geccocrawler.gecco.spider.render.FieldRender;
 import com.geccocrawler.gecco.spider.render.FieldRenderException;
+import com.geccocrawler.gecco.spider.render.Render;
+import com.geccocrawler.gecco.spider.render.RenderContext;
+import com.geccocrawler.gecco.spider.render.RenderException;
+import com.geccocrawler.gecco.spider.render.RenderType;
+import com.geccocrawler.gecco.utils.ReflectUtils;
 
 public class JsonFieldRender implements FieldRender {
 	
@@ -26,7 +35,13 @@ public class JsonFieldRender implements FieldRender {
 		Set<Field> jsonPathFields = ReflectionUtils.getAllFields(bean.getClass(), ReflectionUtils.withAnnotation(JSONPath.class));
 		Object json = JSON.parse(response.getContent());
 		for(Field field : jsonPathFields) {
-			JSONPath JSONPath = field.getAnnotation(JSONPath.class);
+			try {
+				Object value = injectJsonField(request, field, json);
+				fieldMap.put(field.getName(), value);
+			} catch(Exception ex) {
+				throw new FieldRenderException(field, "error : " + json, ex);
+			}
+			/*JSONPath JSONPath = field.getAnnotation(JSONPath.class);
 			String jsonPath = JSONPath.value();
 			Object src = com.alibaba.fastjson.JSONPath.eval(json, jsonPath);
 			if(src == null) {
@@ -37,9 +52,41 @@ public class JsonFieldRender implements FieldRender {
 				fieldMap.put(field.getName(), value);
 			} catch(Exception ex) {
 				throw new FieldRenderException(field, "Conversion error : " + src, ex);
-			}
+			}*/
 		}
 		beanMap.putAll(fieldMap);
+	}
+	
+	private Object injectJsonField(HttpRequest request, Field field, Object json) throws RenderException, FieldRenderException {
+		JSONPath JSONPath = field.getAnnotation(JSONPath.class);
+		String jsonPath = JSONPath.value();
+		Class<?> type = field.getType();//类属性的类
+		Object src = com.alibaba.fastjson.JSONPath.eval(json, jsonPath);
+		boolean isList = ReflectUtils.haveSuperType(type, List.class);//是List类型
+		if(isList) {
+			Type genericType = field.getGenericType();//获得包含泛型的类型
+			Class genericClass = ReflectUtils.getGenericClass(genericType, 0);//泛型类
+			//List<Object>
+			List<SpiderBean> list = new ArrayList<SpiderBean>();
+			JSONArray ja = (JSONArray)src;
+			for(Object jo : ja) {
+				HttpResponse subResponse = HttpResponse.createSimple(jo.toString());
+				Render render = RenderContext.getRender(RenderType.JSON);
+				SpiderBean subBean = render.inject(genericClass, request, subResponse);
+				list.add(subBean);
+			}
+			return list;
+		} else {
+			//Object
+			if(src == null) {
+				throw new FieldRenderException(field, jsonPath + " not found in : " + json);
+			}
+			try {
+				return Conversion.getValue(field.getType(), src);
+			} catch(Exception ex) {
+				throw new FieldRenderException(field, "Conversion error : " + src, ex);
+			}
+		}
 	}
 	
 }
