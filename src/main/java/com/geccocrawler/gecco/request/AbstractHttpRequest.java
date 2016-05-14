ba744627f46dcae2d1c704fd8c81f6e687cf5131
xtuhcy <xtuhcy@163.com>
2016-05-14 12:21:13 +0800

    修改UniqueSpiderScheduler判断唯一性的bug

--- src/main/java/com/geccocrawler/gecco/request/AbstractHttpRequest.java
+++ src/main/java/com/geccocrawler/gecco/request/AbstractHttpRequest.java
@@ -175,7 +175,6 @@ public abstract class AbstractHttpRequest implements HttpRequest, Comparable<Htt
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result	+ ((parameters == null) ? 0 : parameters.hashCode());
 		result = prime * result + ((url == null) ? 0 : url.hashCode());
 		return result;
 	}
--- src/main/java/com/geccocrawler/gecco/scheduler/UniqueSpiderScheduler.java
+++ src/main/java/com/geccocrawler/gecco/scheduler/UniqueSpiderScheduler.java
@@ -7,6 +7,8 @@ import java.util.concurrent.ConcurrentSkipListSet;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.geccocrawler.gecco.request.HttpGetRequest;
+import com.geccocrawler.gecco.request.HttpPostRequest;
 import com.geccocrawler.gecco.request.HttpRequest;
 
 /**
@@ -19,18 +21,17 @@ public class UniqueSpiderScheduler implements Scheduler {
 	
 	private static Log log = LogFactory.getLog(UniqueSpiderScheduler.class);
 	
-	private NavigableSet<HttpRequest> set;
+	private NavigableSet<SortHttpRequest> set;
 	
 	public UniqueSpiderScheduler() {
-		set = new ConcurrentSkipListSet<HttpRequest>(new Comparator<HttpRequest>() {
+		set = new ConcurrentSkipListSet<SortHttpRequest>(new Comparator<SortHttpRequest>() {
 
 			@Override
-			public int compare(HttpRequest o1, HttpRequest o2) {
-				if(o1.hashCode() == o2.hashCode()) {
-					return 0;
-				}
-				if(o1.equals(o2)) {
-					return 0;
+			public int compare(SortHttpRequest o1, SortHttpRequest o2) {
+				if(o1.getHttpRequest().hashCode() == o2.getHttpRequest().hashCode()) {
+					if(o1.getHttpRequest().equals(o2.getHttpRequest())) {
+						return 0;
+					}
 				}
 				return (int)(o1.getPriority() - o2.getPriority());
 			}
@@ -40,7 +41,7 @@ public class UniqueSpiderScheduler implements Scheduler {
 
 	@Override
 	public HttpRequest out() {
-		HttpRequest request = set.pollFirst();
+		HttpRequest request = set.pollFirst().getHttpRequest();
 		if(request != null && log.isDebugEnabled()) {
 			log.debug("OUT:"+request.getUrl()+"(Referer:"+request.getHeaders().get("Referer")+")");
 		}
@@ -49,10 +50,56 @@ public class UniqueSpiderScheduler implements Scheduler {
 
 	@Override
 	public void into(HttpRequest request) {
-		request.setPriority(System.nanoTime());
-		boolean success = set.add(request);
+		boolean success = set.add(new SortHttpRequest(System.nanoTime(), request));
 		if(success && log.isDebugEnabled()) {
 			log.debug("INTO:"+request.getUrl()+"(Referer:"+request.getHeaders().get("Referer")+")");
 		}
+	}
+	
+	private class SortHttpRequest {
+		
+		private long priority;
+		
+		private HttpRequest httpRequest;
+
+		public SortHttpRequest(long priority, HttpRequest httpRequest) {
+			super();
+			this.priority = priority;
+			this.httpRequest = httpRequest;
+		}
+
+		public long getPriority() {
+			return priority;
+		}
+
+		public HttpRequest getHttpRequest() {
+			return httpRequest;
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		UniqueSpiderScheduler uss = new UniqueSpiderScheduler();
+		HttpPostRequest post1 = new HttpPostRequest();
+		post1.setUrl("http://www.1631.com");
+		post1.addField("a", "a");
+		HttpPostRequest post2 = new HttpPostRequest();
+		post2.setUrl("http://www.1632.com");
+		post2.addField("a", "b");
+		uss.into(post1);
+		uss.into(post2);
+		
+		HttpGetRequest get1 = new HttpGetRequest();
+		get1.setUrl("http://www.163.com");
+		get1.addParameter("1", "1");
+		HttpGetRequest get2 = new HttpGetRequest();
+		get2.setUrl("http://www.163.com");
+		uss.into(get1);
+		uss.into(get2);
+		
+		System.out.println(uss.out());
+		System.out.println(uss.out());
+		System.out.println(uss.out());
+		System.out.println(uss.out());
 	}
 }
