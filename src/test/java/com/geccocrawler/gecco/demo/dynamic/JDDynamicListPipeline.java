a590286a1b3abd715636eea32152a32d30a3263d
xtuhcy <xtuhcy@163.com>
2016-07-06 21:16:57 +0800

    JsonPipeline Demo

--- src/test/java/com/geccocrawler/gecco/demo/dynamic/JDDynamicListPipeline.java
+++ src/test/java/com/geccocrawler/gecco/demo/dynamic/JDDynamicListPipeline.java
@@ -5,22 +5,20 @@ import org.apache.commons.lang3.StringUtils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.geccocrawler.gecco.annotation.PipelineName;
-import com.geccocrawler.gecco.pipeline.Pipeline;
+import com.geccocrawler.gecco.pipeline.JsonPipeline;
 import com.geccocrawler.gecco.request.HttpGetRequest;
 import com.geccocrawler.gecco.request.HttpRequest;
 import com.geccocrawler.gecco.scheduler.SchedulerContext;
-import com.geccocrawler.gecco.spider.SpiderBean;
 
 @PipelineName("jdDynamicListPipeline")
-public class JDDynamicListPipeline implements Pipeline<SpiderBean> {
+public class JDDynamicListPipeline extends JsonPipeline {
 
 	/**
 	 * 由于bean是动态生成，pipline的process方法只能传入SpiderBean类型，一般通过将bean转换为json对象进行处理
 	 * 
 	 */
 	@Override
-	public void process(SpiderBean bean) {
-		JSONObject jo = JSON.parseObject(JSON.toJSONString(bean));
+	public void process(JSONObject jo) {
 		HttpRequest currRequest = (HttpGetRequest)JSON.toJavaObject(jo.getJSONObject("request"), HttpGetRequest.class);
 		//下一页继续抓取
 		int currPage = jo.getIntValue("currPage");
