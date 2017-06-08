adbf1c79308c40251d0c828e3be53c285e00be34
xtuhcy <xtuhcy@163.com>
2017-06-08 09:45:30 +0800

    格式修改

--- src/main/java/com/geccocrawler/gecco/downloader/proxy/ProxysContext.java
+++ src/main/java/com/geccocrawler/gecco/downloader/proxy/ProxysContext.java
@@ -1,15 +1,24 @@
 package com.geccocrawler.gecco.downloader.proxy;
 
+import com.geccocrawler.gecco.spider.Spider;
 import com.geccocrawler.gecco.spider.SpiderThreadLocal;
 
 public class ProxysContext {
 	
 	public static Proxys get() {
-		return SpiderThreadLocal.get().getEngine().getProxysLoader();
+		Spider spider = SpiderThreadLocal.get();
+		if(spider == null) {
+			return null;
+		}
+		return spider.getEngine().getProxysLoader();
 	}
 	
 	public static boolean isEnableProxy() {
-		return SpiderThreadLocal.get().getEngine().isProxy();
+		Spider spider = SpiderThreadLocal.get();
+		if(spider == null) {
+			return false;
+		}
+		return spider.getEngine().isProxy();
 	}
 
 }
